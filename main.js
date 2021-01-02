141d4eff23a14dc57c3a5163dd28468afa60a8cb
Ngomeli <joshuangomeli@gmail.com>
2021-01-02 05:54:06 +0300

    added the features section

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 var menuBtn = document.getElementById("menuBtn");
 var sideNav = document.getElementById("sideNav");
 var menu = document.getElementById("menu");
-sideNav.style.right == "-250px";
+sideNav.style.right == "-250px"; /*the nav is hidden in the webpage*/
 menuBtn.onclick = function() {
     if (sideNav.style.right == "-250px") {
-        sideNav.style.right = "0";
-        menu.src = "images/close.png";
+        sideNav.style.right = "0"; /*the navbar appears on the web page*/
+        menu.src = "images/close.png"; /*the close image shows up*/
     } else {
         sideNav.style.right = "-250px";
-        menu.src = "images/menu.png";
+        menu.src = "images/menu.png"; /*the open image shows up*/
     }
 }
